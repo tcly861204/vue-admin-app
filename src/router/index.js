54847c93b997bad3de80a8aeb80f55592ba88566
tcly861204 <tcly861204@hotmail.com>
2019-09-02 23:04:22 +0800

    merge

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-
-Vue.use(Router)
+import Home from '@/pages/Home.vue'
+process.env.NODE_ENV === "development" ? Vue.use(Router) : null;
 
 export default new Router({
   mode: 'history',
@@ -16,7 +15,7 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ '@/pages/About.vue')
     }
   ]
 })
