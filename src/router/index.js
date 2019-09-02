232f3ff5e03158f9935708187d92c96bf489fd10
tcly861204 <tcly861204@hotmail.com>
2019-09-02 23:25:48 +0800

    merge

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/Home.vue'
 process.env.NODE_ENV === "development" ? Vue.use(Router) : null;
-
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
@@ -11,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import(/* webpackChunkName: "login" */ '@/pages/login.vue')
     },
     {
       path: '/about',
