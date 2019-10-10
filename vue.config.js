651c22a31c2e3ba6fe03f633e747819b2ce8f385
tcly861204 <157@op110.com>
2019-10-10 16:40:04 +0800

    merge

--- vue.config.js
+++ vue.config.js
@@ -1,52 +1,49 @@
 require('events').EventEmitter.defaultMaxListeners = 0 // 处理爆栈警告
-const TerserPlugin = require('terser-webpack-plugin')  // 用于在生成环境剔除debuger和console
-const CompressionPlugin = require("compression-webpack-plugin") // gzip压缩,优化http请求,提高加载速度
+const TerserPlugin = require('terser-webpack-plugin') // 用于在生成环境剔除debuger和console
+const CompressionPlugin = require('compression-webpack-plugin') // gzip压缩,优化http请求,提高加载速度
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer') // 代码分析工具
 const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin')
 const AutoDllPlugin = require('autodll-webpack-plugin')
 const webpack = require('webpack')
-const path = require('path');
+const path = require('path')
 const resolve = dir => {
-  return path.join(__dirname, dir);
+  return path.join(__dirname, dir)
 };
 
 const env = process.env.NODE_ENV
-let target = process.env.VUE_APP_URL  // development和production环境是不同的
+let target = process.env.VUE_APP_URL // development和production环境是不同的
 
 const cdn = {
   // 开发环境
   dev: {
-      css: [],
-      js: [
-      ]
+    css: [],
+    js: []
   },
   // 生产环境
   build: {
-      css: [
-        'https://cdn.bootcss.com/element-ui/2.11.1/theme-chalk/index.css',
-        'https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.css'
-      ],
-      js: [
-        'https://cdn.bootcss.com/vue/2.6.10/vue.min.js',
-        'https://cdn.bootcss.com/vue-router/3.1.2/vue-router.min.js',
-        'https://cdn.bootcss.com/vuex/2.3.1/vuex.min.js',
-        'https://cdn.bootcss.com/axios/0.19.0/axios.min.js',
-        'https://cdn.bootcss.com/vue-i18n/8.13.0/vue-i18n.min.js',
-        'https://cdn.bootcss.com/element-ui/2.11.1/index.js',
-        'https://cdn.bootcss.com/echarts/3.8.5/echarts.min.js',
-        'https://cdn.bootcss.com/Mock.js/1.0.1-beta3/mock-min.js',
-        'https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.js',
-        'https://cdn.bootcss.com/js-cookie/2.2.0/js.cookie.min.js'
-      ]
+    css: [
+      'https://cdn.bootcss.com/element-ui/2.11.1/theme-chalk/index.css',
+      'https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.css'
+    ],
+    js: [
+      'https://cdn.bootcss.com/vue/2.6.10/vue.min.js',
+      'https://cdn.bootcss.com/vue-router/3.1.2/vue-router.min.js',
+      'https://cdn.bootcss.com/vuex/2.3.1/vuex.min.js',
+      'https://cdn.bootcss.com/axios/0.19.0/axios.min.js',
+      'https://cdn.bootcss.com/vue-i18n/8.13.0/vue-i18n.min.js',
+      'https://cdn.bootcss.com/element-ui/2.11.1/index.js',
+      'https://cdn.bootcss.com/echarts/3.8.5/echarts.min.js',
+      'https://cdn.bootcss.com/Mock.js/1.0.1-beta3/mock-min.js',
+      'https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.js',
+      'https://cdn.bootcss.com/js-cookie/2.2.0/js.cookie.min.js'
+    ]
   }
 }
-
-
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-admin-app/" : "/",
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-admin-app/' : '/',
   outputDir: './dist',
-  assetsDir:'static',
-  filenameHashing:true, // false 来关闭文件名哈希
+  assetsDir: 'static',
+  filenameHashing: true, // false 来关闭文件名哈希
   lintOnSave: false, // 关闭eslint
   // 打包时不生成.map文件
   productionSourceMap: false,
@@ -73,78 +70,78 @@ module.exports = {
     config.performance
       .set('hints', false)
     // 移除 prefetch 插件
-    config.plugins.delete("prefetch");
+    config.plugins.delete('prefetch');
     // 移除 preload 插件
     config.plugins.delete('preload');
     // 压缩代码
     config.optimization.minimize(true);
     // 分割代码
     config.optimization.splitChunks({
-        chunks: 'all'
+      chunks: 'all'
     })
     // 对图片进行压缩处理
     config.module
-    .rule('images')
-    .use('image-webpack-loader')
-    .loader('image-webpack-loader')
-    .options({
+      .rule('images')
+      .use('image-webpack-loader')
+      .loader('image-webpack-loader')
+      .options({
         disable: true, // webpack@2.x and newer
         quality: '65-80',
         speed: 4
-    })
-    .end()
+      })
+      .end()
     // 添加CDN参数到htmlWebpackPlugin配置中， 详见public/index.html 修改
     config
-    .plugin('html')
-    .tap(args => {
-      if (process.env.NODE_ENV === 'production') {
+      .plugin('html')
+      .tap(args => {
+        if (process.env.NODE_ENV === 'production') {
           args[0].cdn = cdn.build
-      }
-      if (process.env.NODE_ENV === 'development') {
+        }
+        if (process.env.NODE_ENV === 'development') {
           args[0].cdn = cdn.dev
-      }
-      return args
-    })
+        }
+        return args
+      })
   },
-  configureWebpack:config => {
+  configureWebpack: config => {
     // 为生产环境修改配置...
     if (process.env.NODE_ENV === 'production') {
       // 忽略生产环境打包的文件
       config.externals = {
-        "vue": "Vue",
-        "vue-router": "VueRouter",
-        "vuex": "Vuex",
-        "vue-i18n": "VueI18n",
-        "axios": "axios",
+        'vue': 'Vue',
+        'vue-router': 'VueRouter',
+        'vuex': 'Vuex',
+        'vue-i18n': 'VueI18n',
+        'axios': 'axios',
         'element-ui': 'ELEMENT',
-        'echarts':'echarts',
-        'mockjs':'Mock',
-        'nprogress':'NProgress',
-        'js-cookie':'Cookies'
+        'echarts': 'echarts',
+        'mockjs': 'Mock',
+        'nprogress': 'NProgress',
+        'js-cookie': 'Cookies'
       }
-      // 去除console来减少文件大小，效果同'UglifyJsPlugin'
-      new TerserPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: false, // Must be set to true if using source-maps in production
-        terserOptions: {
-          compress: {
-            warnings: false,
-            drop_console: true,
-            drop_debugger: true,
-            pure_funcs: ['console.log']
-          }
-        }
-      })
       // 开启gzip压缩
       config.plugins.push(
+        // 去除console来减少文件大小，效果同'UglifyJsPlugin'
+        new TerserPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: false, // Must be set to true if using source-maps in production
+          terserOptions: {
+            compress: {
+              warnings: false,
+              drop_console: true,
+              drop_debugger: true,
+              pure_funcs: ['console.log']
+            }
+          }
+        }),
         new CompressionPlugin({
           algorithm: 'gzip',
-          test: new RegExp("\\.(" + ["js", "css"].join("|") + ")$"), // 匹配文件扩展名
+          test: new RegExp('\\.(' + ['js', 'css'].join('|') + ')$'), // 匹配文件扩展名
           threshold: 5120, // 对超过5k的数据进行压缩
           minRatio: 0.8,
           cache: true, // 是否需要缓存
-          deleteOriginalAssets: false  // true删除源文件(不建议);false不删除源文件
+          deleteOriginalAssets: false // true删除源文件(不建议);false不删除源文件
         }),
         new BundleAnalyzerPlugin({
           openAnalyzer: false,
@@ -176,7 +173,7 @@ module.exports = {
       )
     }
   },
-   // 第三方插件配置
+  // 第三方插件配置
   pluginOptions: {
 
   }
