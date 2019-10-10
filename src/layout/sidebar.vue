3f68d249b8a4816c0bdd2edfd1f3f207ea000bbd
tcly861204 <157@op110.com>
2019-10-10 16:24:09 +0800

    merge

--- src/layout/sidebar.vue
+++ src/layout/sidebar.vue
@@ -9,7 +9,7 @@
   <el-submenu index="1">
     <template slot="title">
       <i class="el-icon-location"></i>
-      <span>导航一</span>
+      <span>首页</span>
     </template>
     <el-menu-item-group>
       <template slot="title">分组一</template>
--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ import router from './router'
 import ElementUI from 'element-ui'
 import '@/mockjs'
 // i18n国际化
-import i18n from "@/local"
-if (process.env.NODE_ENV === "development") {
+import i18n from '@/local'
+if (process.env.NODE_ENV === 'development') {
   require('element-ui/lib/theme-chalk/index.css')
 }
-Vue.use(ElementUI, { size: 'mini'})
+Vue.use(ElementUI, { size: 'mini' })
 Vue.config.productionTip = false
-import './theme/blue/main.less'
+require('./theme/blue/main.less')
 new Vue({
   router,
   i18n,
